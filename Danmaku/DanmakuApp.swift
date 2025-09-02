3f4a11179040b95899e1a22665105bfa89f857dc
MAA39 <masa.nekoshinshi39@gmail.com>
2025-09-02 14:44:47 +0900

    Initial Commit

---
+++ Danmaku/DanmakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanmakuApp.swift
+//  Danmaku
+//
+//  Created by maa on 2025/09/02.
+//
+
+import SwiftUI
+
+@main
+struct DanmakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DanmakuTests/DanmakuTests.swift
@@ -0,0 +1,17 @@
+//
+//  DanmakuTests.swift
+//  DanmakuTests
+//
+//  Created by maa on 2025/09/02.
+//
+
+import Testing
+@testable import Danmaku
+
+struct DanmakuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
